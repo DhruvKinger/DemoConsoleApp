a816d71a09c44e0464e53d7255f7756da189c6b2
Dhruv Kinger <cse1785@gmail.com>
2022-01-14 11:09:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace DemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputfile = Path.Combine(Environment.CurrentDirectory, "Names.txt");
+           
+            var Names = new StreamReader(inputfile);
+
+            Console.SetIn(Names);
+
+            string readvalue = Console.ReadLine();
+            while(readvalue !=null)
+            {
+                Console.WriteLine("Value:-" + readvalue);
+                readvalue = Console.ReadLine();
+            }
+
+            Console.WriteLine("Press Enter");
+            //string s;
+            //do
+            //{
+            //    s = Console.ReadLine();
+
+            //    Console.WriteLine("Entered:-"+ s);
+
+            //} while (s != null);
+            //Console.WriteLine("What's your name ?");
+
+            //var name =Console.ReadLine();
+
+            //Console.WriteLine(name);
+
+            //Console.ReadLine(); // FOR A PAUSE
+        }
+    }
+}
